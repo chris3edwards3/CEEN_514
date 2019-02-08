3422beabb22b096e099cbb31ebf1a9d9a04e2b0a
chris3edwards3 <chris3edwards3@gmail.com>
2019-02-07 17:46:01 -0700

    Update lengthconverter.js

--- lengthconverter.js
+++ lengthconverter.js
@@ -1,13 +1,18 @@
-function convertUnits() {
-  const inputValue = document.getElementById("value")  
-  const fromUnit = document.getElementById("from")
-  const toUnit = document.getElementById("to")
+
+function linearConversion() {
+  let inputValue = document.getElementById("value").value; 
+  let fromUnit = document.getElementById("fromUnit").value;
+  let toUnit = document.getElementById("toUnit").value;
   
   let listUnits = ["inches", "feet", "yards", "miles", "meters", "kilometers"];
 
-    // The array below was copied from Wade Roberts. 
-	const convRates = [
-        [1.0, 0.0833333, 0.0277778, 1.57828e-5, 0.0254, 2.54e-5],
+    // The array below was given to me by Wade Roberts. 
+	// The top row is the converstions from in to in, ft, yd, mi, m, and km, respectively.
+	// Subsequent rows follow the same idea.
+	// The order of the rows and columns matches "listUnits"
+	
+	let convRates = [
+        [1.0, 0.083333, 0.027778, 1.57828e-5, 0.0254, 2.54e-5],
         [12.0, 1.0, 0.333333, 0.000189394, 0.3048, 0.0003048],
         [36.0, 3.0, 1.0, 0.000568182, 0.9144, 0.0009144],
         [63360.0, 5280.0, 1760.0, 1.0, 1609.34, 1.60934],
@@ -19,10 +24,9 @@ function convertUnits() {
 
     let answer = (inputValue * convFactor);
 
+	// This converts the answer into the text string I want to display:
+	// eg: 24 inches = 2 feet
     let answerText = `${inputValue} ${listUnits[fromUnit]} = ${answer} ${listUnits[toUnit]}`;
-
-    let fullResult = `<h2>${answerText}</h2>`;
 	
-	document.getElementById("finalAnswer").innerHTML = fullResult;
-    
+	document.getElementById("finalAnswer").innerHTML = answerText;
 }
