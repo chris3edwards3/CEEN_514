9552d74663e5f23648623019196e050065650b18
chrisedwards <chris3edwards3@gmail.com>
2019-02-27 22:32:38 -0500

    Does adding the layers work?

--- javascript/esrisnowmelt.js
+++ javascript/esrisnowmelt.js
@@ -1,9 +1,15 @@
 require([
-	"esri/Map", 
-	"esri/views/MapView"
+	"esri/Map",
+	"esri/views/MapView",
+	"esri/Graphic", // This part thanks to Kyler Ashby
+	"esri/layers/MapImageLayer",
+	"esri/layers/support/Sublayer"
 ], function(
 	Map,
-	MapView
+	MapView,
+	Graphic,
+	MapImageLayer,
+	Sublayer
 ) {
 	const map = new Map({
 		basemap: "osm"
@@ -12,14 +18,15 @@ require([
 	const view = new MapView({
 		container: "viewDiv",
 		map: map,
-		zoom: 2,
+		zoom: 7,
 		center: {
-			latitude: 0,
-			longitude: 0
+			latitude: 39.5,
+			longitude: -111.5
 		}
 	});
 
-	
-							
-
+    var layer = new MapImageLayer({
+        url: "http://geoserver2.byu.edu/arcgis/rest/services/3_Toed_Sloth/Snowmelt/MapServer"
+    });
+    map.add(layer);  // adds the layer to the map
 });
